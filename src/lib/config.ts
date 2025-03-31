a42441c452c14d5f2e9148a8aabd95b12a01ff66
garrettgalow <garrett.galow@gmail.com>
2025-03-30 20:31:12 -0500

    Add currency enum and table

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -11,5 +11,29 @@ export const config = {
     "Standard",
     "Hardcore",
     "DotH - coming soon"
-  ]
+  ],
+
+  currencyItems: [
+    "Orb of Transmutation",
+    "Orb of Augmentation",
+    "Orb of Alchemy",
+    "Regal Orb",
+    "Chaos Orb",
+    "Vaal Orb",
+    "Exalted Orb",
+    "Divine Orb",
+    "Orb of Annulment",
+    "Orb of Chance",
+    "Mirror of Kalandra",
+    "Lesser Jewellers Orb",
+    "Greater Jewellers Orb",
+    "Perfect Jewellers Orb",
+    "Blacksmiths Whetstone",
+    "Arcanists Etcher",
+    "Armourers Scrap",
+    "Gemcutters Prism",
+    "Glassblowers Bauble",
+    "Artificer's Orb"
+]
+
 }; 
